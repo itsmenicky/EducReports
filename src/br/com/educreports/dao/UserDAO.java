1bea8fde3343c0ebf8330bcde73bdd90cfdc7bdc
itsmenicky <nick104@outlook.com>
2024-07-09 18:03:29 -0300

    doc: license

--- src/br/com/educreports/dao/UserDAO.java
+++ src/br/com/educreports/dao/UserDAO.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2024 Nickolas Martins
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package br.com.educreports.dao;
 import br.com.educreports.dal.ConnectionModule;
 import br.com.educreports.models.User;
 import br.com.educreports.services.passwordCrypt;
-import br.com.educreports.services.sendEmail;
-import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import java.nio.charset.StandardCharsets;
@@ -11,7 +27,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Random;
 
 public class UserDAO {
     private Connection conexao;
